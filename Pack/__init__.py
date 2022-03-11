1c8d3c588d45c830b038b85d560ffe801f3127f4
Anikey Semenov <93664553+AnikeyPS@users.noreply.github.com>
2022-03-11 13:59:16 +0300

    Update __init__.py

--- Pack/__init__.py
+++ Pack/__init__.py
@@ -15,7 +15,7 @@ def outstyle(text: str, style_name: str, text_start: str='', text_stop: str=''):
     return f"{text_start}\033[{constants.DATA_OUTSTYLE[style_name.replace(' ', '_')]}m{text}\033[0m\033[37m{text_stop}"
 
 
-def randomtext(max_signs: int=100):
+def randomstring(max_signs: int=100):
     text = ''
     for i in range(randint(1, max_signs) + 1):
         text = text + choice(constants.DATA_RANDOMTEXT)
