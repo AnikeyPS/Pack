cba688d45fd7ee40229ab6dc8a9681c8e5c6a4ce
Anikey Semenov <93664553+AnikeyPS@users.noreply.github.com>
2022-03-11 09:25:21 +0300

    Create constants.py

---
+++ Pack/constants.py
@@ -0,0 +1,2 @@
+DATA_outstyle = {"black": "30", "back_black": "40", "red": "31", "back_red": "41", "green": "32", "back_green": "42", "yellow": "33", "back_yellow": "43", "blue": "34", "back_blue": "44", "purple": "35", "back_purple": "45", "turquoise": "36", "back_turquoise": "46", "white": "37", "back_white": "47", "fatty": "1", "faded": "2", "italics": "3", "uderlined": "4",	
+"rare_blinking": "5", "fast_flashing": "6"}
