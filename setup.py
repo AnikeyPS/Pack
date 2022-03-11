7277dc4d38d9e2435a020b6392701f5f2ef25de5
Anikey Semenov <93664553+AnikeyPS@users.noreply.github.com>
2022-03-11 08:31:49 +0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='Pack',
+      version='0.1',
+      description='Python Pack',
+      author='Anikey',
+      author_email='semenov.anikey@gmail.com',
+      packages=['Pack'],
+     )
