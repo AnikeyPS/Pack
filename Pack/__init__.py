faadc55e234af98f0c2b6d53036e2056b6ebf9f0
Anikey Semenov <93664553+AnikeyPS@users.noreply.github.com>
2022-03-11 17:44:29 +0300

    Update __init__.py

--- Pack/__init__.py
+++ Pack/__init__.py
@@ -4,10 +4,12 @@ from . import constants
 from random import choice, randint
 
 
-def saveimgbyurl(image_url: str, file_name: str):
+def saveimgbyurl(image_url: str, file_name: str=None):
     image = request('get', image_url)
-    image_type = image_url.split('.')[-1]
-    with open(f"{file_name}.{image_type}", 'wb') as f:
+    image_type = image_url.split('.')
+    if file_name is None:
+        file_name = image_type[-2].split("/")[-1]
+    with open(f"{file_name}.{image_type[-1]}", 'wb') as f:
         f.write(image.content)
 
 
