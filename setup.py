91fcf2e00af30f90f6d22e242c948ce880eee6b8
Anikey Semenov <93664553+AnikeyPS@users.noreply.github.com>
2022-03-11 08:36:28 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,4 @@
-from Pack.core import setup
-
+from distutils.core import setup
 setup(name='Pack',
-      version='0.1',
-      description='Python Pack',
-      author='Anikey',
-      author_email='semenov.anikey@gmail.com',
-      packages=['Pack'],
-     )
+      version='1.0',
+      )
