162769ecb3e7827955a6e037e86cdc856fe185a8
Anikey Semenov <93664553+AnikeyPS@users.noreply.github.com>
2022-03-11 13:50:30 +0300

    Update __init__.py

--- Pack/__init__.py
+++ Pack/__init__.py
@@ -1,6 +1,7 @@
-# Utils-pack 1.0
+# Utils-pack 1.3.2
 from requests import request
 from . import constants
+from random import choice, randint
 
 
 def saveimgbyurl(image_url: str, file_name: str):
@@ -14,7 +15,7 @@ def outstyle(text: str, style_name: str, text_start: str='', text_stop: str=''):
     return f"{text_start}\033[{constants.DATA_OUTSTYLE[style_name.replace(' ', '_')]}m{text}\033[0m\033[37m{text_stop}"
 
 
-def randomtext(max_signs):
+def randomtext(max_signs: int=100):
     text = ''
     for i in range(randint(1, max_signs) + 1):
         text = text + choice(constants.DATA_RANDOMTEXT)
