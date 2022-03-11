9f508e25cc55d805b2c51fc2fba54d4da09b69af
Anikey Semenov <93664553+AnikeyPS@users.noreply.github.com>
2022-03-11 09:39:36 +0300

    Update __init__.py

--- Pack/__init__.py
+++ Pack/__init__.py
@@ -1,6 +1,6 @@
 # Utils-pack 1.0
 from requests import request
-import constants
+from . import constants
 
 
 def saveimgbyurl(image_url: str, file_name: str):
@@ -11,4 +11,4 @@ def saveimgbyurl(image_url: str, file_name: str):
 
 
 def outstyle(text: str, style_name: str, text_start: str='', text_stop: str=''):
-    return f"{text_start}\033[{constants.DATA_OUTSTYLE[style_name.replace(" ", "_")]}m{text}\033[0m\033[37m{text_stop}"
+    return f"{text_start}\033[{DATA_OUTSTYLE[style_name.replace(' ', '_')]}m{text}\033[0m\033[37m{text_stop}"
