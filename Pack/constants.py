205ec9cfe1c549044db0a17579ad9fbd73e4d80d
Anikey Semenov <93664553+AnikeyPS@users.noreply.github.com>
2022-03-11 13:38:02 +0300

    Update constants.py

--- Pack/constants.py
+++ Pack/constants.py
@@ -1,2 +1,4 @@
 DATA_OUTSTYLE = {"black": "30", "back_black": "40", "red": "31", "back_red": "41", "green": "32", "back_green": "42", "yellow": "33", "back_yellow": "43", "blue": "34", "back_blue": "44", "purple": "35", "back_purple": "45", "turquoise": "36", "back_turquoise": "46", "white": "37", "back_white": "47", "fatty": "1", "faded": "2", "italics": "3", "uderlined": "4",	
 "rare_blinking": "5", "fast_flashing": "6"}
+
+DATA_RANDOMTEXT = ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'z', 'x', 'c', 'v', 'b', 'n', 'm', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
