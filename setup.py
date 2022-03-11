9e17439cd3265a149634137f94783cbed1ad1ed4
Anikey Semenov <93664553+AnikeyPS@users.noreply.github.com>
2022-03-11 18:05:12 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,2 +1,2 @@
 from distutils.core import setup
-setup(name='Utils-pack', version='1.3.8', packages=['Utils_pack'], package_dir={'Utils_pack': 'Pack'}, install_requires=["requests"])
+setup(name='Utils-pack', version='1.3.8', packages=['utils_pack'], package_dir={'utils_pack': 'Pack'}, install_requires=["requests"])
