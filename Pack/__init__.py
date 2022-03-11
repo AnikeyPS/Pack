bd82c87b7eeb779ed8e7ad8f66f029d0f851cb84
Anikey Semenov <93664553+AnikeyPS@users.noreply.github.com>
2022-03-11 09:28:50 +0300

    Update __init__.py

--- Pack/__init__.py
+++ Pack/__init__.py
@@ -10,5 +10,5 @@ def saveimgbyurl(image_url: str, file_name: str):
         f.write(image.content)
 
 
-def outstyle(text: str, style_id: int, text_start: str='', text_stop: str=''):
-    return f"{text_start}\033[{constants.}m{text}\033[0m\033[37m{text_stop}"
+def outstyle(text: str, style_name: str, text_start: str='', text_stop: str=''):
+    return f"{text_start}\033[{constants.DATA_OUTSTYLE[style_name.replace(" ", "_")]}m{text}\033[0m\033[37m{text_stop}"
